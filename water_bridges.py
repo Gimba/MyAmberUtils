a9ecbaeafa225d4a6ad043300d33d687ae8be4c2
Martin Rosellen <Martin.Rosellen@fu-berlin.de>
2019-01-23 15:18:02 +0000

    init

---
+++ water_bridges.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+# Copyright (c) 2019 Martin Rosellen
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+# Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import argparse
+import sys
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='Script to check for hydrogen binding between watermolecules between 2 given residue masks.')
+    parser.add_argument('parm', help='parm file')
+    parser.add_argument('trajin', help='trajectory')
+    parser.add_argument('mask1', help='mask 1')
+    parser.add_argument('mask2', help='mask 2')
+
+
+if __name__ == '__main__':
+    main(sys.argv)
