a4bde5afc69dc2cffcb4befc6ad886c790d60a57
Martin Rosellen <martin.rosellen@fu-berlin.de>
2019-08-12 13:09:10 +0100

    resolved conflict and moved logic into method block

--- atom_from_res.py
+++ atom_from_res.py
@@ -5,15 +5,18 @@ import argparse
 from os import remove
 
 
-def main(argv):
-    parser = argparse.ArgumentParser(description='Add a chain identifier to specified residue range')
-    parser.add_argument('pdb', help='pdb file name')
-    parser.add_argument('res', help='residue')
-    parser.add_argument('atom_type', help='atom type')
+def atom_from_res(pdb, res, atom_type):
+    """
+    Returns the atom number in the pdb file of a given residue with number 'res' and type 'atom_type'
 
-    args = parser.parse_args()
+    e.g. atom_from_res('WT.pdb', '29', 'CA')
 
-    with open(args.pdb, 'r') as f:
+    :param pdb: file in pdb format
+    :param res: residue number
+    :param atom_type: atom type
+    :return: atom numer
+    """
+    with open(pdb, 'r') as f:
         for line in f.readlines():
             line = line.split()
 
@@ -23,10 +26,21 @@ def main(argv):
             else:
                 res_column = 5
 
-            if len(line) > 5 and line[res_column] == args.res and line[2] == args.atom_type:
+            if len(line) > 5 and line[res_column] == res and line[2] == atom_type:
                 print(' '.join(line))
                 print('Atom number: {}'.format(line[1]))
 
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='Add a chain identifier to specified residue range')
+    parser.add_argument('pdb', help='pdb file name')
+    parser.add_argument('res', help='residue')
+    parser.add_argument('atom_type', help='atom type')
+
+    args = parser.parse_args()
+
+    atom_from_res(args.pdb, args.res, args.atom_type)
+
+
 if __name__ == '__main__':
     main(sys.argv)
-
