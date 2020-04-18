a4a5b6c83e28f96ced54da932731a0f58c70c5e3
Martin Rosellen <martin.rosellen@fu-berlin.de>
2020-04-18 16:09:14 +0100

    implemented parser

--- clean_pdb.py
+++ clean_pdb.py
@@ -1,8 +1,11 @@
 import sys
+import argparse
 
 
 def main(args):
-    pass
+    parser = argparse.ArgumentParser(description='Resolve issues with pdb numbering.')
+    parser.add_argument('pdb_file', help='pdb_file')
+    args = parser.parse_args()
 
 
 if __name__ == '__main__':
