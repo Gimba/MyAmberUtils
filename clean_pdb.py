fea871398741482c216f814006a62095d4a46926
Martin Rosellen <martin.rosellen@fu-berlin.de>
2020-04-18 16:19:33 +0100

    added method to fix residues including a letter

--- clean_pdb.py
+++ clean_pdb.py
@@ -1,5 +1,20 @@
 import sys
 import argparse
+import re
+
+
+def reindex_ABC_residues(lst):
+    clean_list = []
+    index_increment = 0
+    for k in lst:
+        if k.isdecimal():
+            clean_list.append(int(k) + index_increment)
+
+        else:
+            index_increment += 1
+            res_id = int(re.findall(r'\d+', k)[0]) + index_increment
+            clean_list.append(res_id)
+    return clean_list
 
 
 def main(args):
