ee1dece54087628ad9f6a24eec62c63b90631776
Martin Rosellen <martin.rosellen@fu-berlin.de>
2020-04-18 16:36:14 +0100

    reading in residues and sorting

--- clean_pdb.py
+++ clean_pdb.py
@@ -22,6 +22,28 @@ def main(args):
     parser.add_argument('pdb_file', help='pdb_file')
     args = parser.parse_args()
 
+    file = args.pdb_file
+    residues = {}
+    # seqres = {}
+    with open(file, 'r') as f:
+        for line in f.readlines():
+            # if line[:6] == 'SEQRES':
+            #     line = line.split()
+            #     if line[2] in seqres.keys():
+            #         seqres[line[2]].extend(line[4:])
+            #     else:
+            #         seqres[line[2]] = line[4:]
+
+            if line[:4] == 'ATOM':
+                if line[21] in residues.keys():
+                    residues[line[21]].extend([line[22:27]])
+                else:
+                    residues[line[21]] = [line[22:27]]
+    for k, v in residues.items():
+        v = list(set(v))
+        v = sorted(v, key=lambda x: (int(x[:-1]), x[-1]))
+        residues[k] = v
+
 
 if __name__ == '__main__':
     main(sys.argv)
